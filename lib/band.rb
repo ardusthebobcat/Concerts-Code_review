60a9f7d5b5a9b45b9b6b93319ed1a963b799af9b
Michael Summers <student@epicodus.com>
2015-08-28 10:06:46 -0700

    Add titlecase callback to Band. '

--- lib/band.rb
+++ lib/band.rb
@@ -1,5 +1,9 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many(:venues)
   validates(:name, {:presence => true})
+  before_save(:titlecase)
 
+  define_method(:titlecase) do
+    self.name = name().titleize()
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,13 +1,21 @@
 require('spec_helper')
 
+##############################################################################################################
+# Its a weird friday. Naming things based on the music Im listening to instead of a regular theme. Sorry ^_^ #
+##############################################################################################################
 describe(Band) do
   it("Checks ActiveRecord success") do
-    test_band = Band.create({:name => "Dethklok", :description => "A multi-krillion dollar death-metal band, famed for their totally metal music and for awakening a troll that destroyed Norway.", :album => "The Doomstar Requiem"})
+    test_band = Band.create({:name => "Dethklok", :description => "A multi-krillion dollar death-metal band, famed for their totally metal music and for awakening a troll that destroyed Norway.", :album => "The Dethalbum"})
     expect(test_band.name()).to(eq("Dethklok"))
   end
 
   it("tests name-presence validation") do
     test_band = Band.new({:name => "", :description => "A multi-krillion dollar death-metal band, famed for their totally metal music and for awakening a troll that destroyed Norway.", :album => "The Doomstar Requiem"})
     expect(test_band.save()).to(eq(false))
+  end
+
+  it("tests name-titlecase callback") do
+    test_band = Band.create({:name => "dethklok", :description => "A multi-krillion dollar death-metal band, famed for their totally metal music and for awakening a troll that destroyed Norway.", :album => "The Doomstar Requiem"})
+    expect(test_band.name()).to(eq("Dethklok"))
   end
 end
